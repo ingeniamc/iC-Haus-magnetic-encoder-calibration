473e2c9a6fac4a84c07e06d5f617a8f3b3059e47
lucia.teruel <lteruel96@gmail.com>
2026-08-19 12:50:23 +0200

    DR3256AC-796 Fix None msg

--- __main__.py
+++ __main__.py
@@ -260,11 +260,13 @@ def main() -> None:
     all_ok = True
     for enc_num, result in results.items():
         status = "SUCCESS" if result.success else "FAILED"
-        logger.info(
-            f"Encoder {enc_num}: {status} ({result.iterations} iterations) "
-            f"(InRange %: Max={result.nonius_in_range_max:.2f}%, "
-            f"Min={result.nonius_in_range_min:.2f}%)"
-        )
+        result_msg = f"Encoder {enc_num}: {status} ({result.iterations} iterations) "
+        if result.success:
+            result_msg += (
+                f"(InRange %: Max={result.nonius_in_range_max:.2f}%, "
+                f"Min={result.nonius_in_range_min:.2f}%)"
+            )
+        logger.info(result_msg)
         if not result.success:
             all_ok = False
 
